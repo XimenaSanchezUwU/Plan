314b81ed4060c4bdce755fc36a60b4b9343795b4
XimenaSanchezUwU <126804482+XimenaSanchezUwU@users.noreply.github.com>
2023-06-09 21:58:04 -0600

    Add files via upload

---
+++ PlanDeVida_2023_06_10_03_55_24/sketch.js
@@ -0,0 +1,26 @@
+let img;
+let myFont;
+function preload() {
+  img = loadImage('assets/Plan.png');
+  myFont = loadFont('assets/Warung Kopi Regular.otf');
+}
+
+function setup() {
+  var cnv = createCanvas(windowWidth, windowHeight);
+  cnv.style('display', 'block');
+  textFont(myFont);
+}
+
+function draw() {
+  background(220);
+  image(img, 0, 0, windowWidth, windowHeight);
+  for (let i = 0; 9 > i; i++){
+    noStroke();
+    textSize(80);
+    fill(196*i, 13*i, 222*i);
+    //ellipse(120*1, 35*i, 20*i, 20);
+    text('¿Cuál es tu plan de vida?', 300, random(350*i,322));
+    frameRate(18);
+  }
+  
+}
